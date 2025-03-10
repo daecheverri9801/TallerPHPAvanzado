9b955b16ac14ba564d24bdf05fe9e55d78162ca1
Daniel Arango Echeverri <daniel.arango20125@ucaldas.edu.co>
2025-03-10 15:29:29 -0500

    Ajuste

--- resources/views/emails/resultado_interes.blade.php
+++ resources/views/emails/resultado_interes.blade.php
@@ -8,6 +8,7 @@
 <body>
     <h2>Resultado del Cálculo de Interés Compuesto</h2>
     <p>El resultado del cálculo es: <strong>{{ $resultado }}</strong></p>
+    <p>El correo ha sido enviado con exito!</p>
 </body>
 
 </html>
